d73e6539713e3fc7e7cc1c0c4cc02caba5b7cbcc
imrshu <rishabh.verma11998@gmail.com>
2017-08-12 20:05:22 +0530

    Add Model Data

---
+++ js/model.js
@@ -0,0 +1,76 @@
+// Contains the list of Venues or Places
+var model = [
+  {
+    "name": "Red Fort",
+    "venue_id": "4bf214b83506ef3b1d7abd22",
+    "location":{
+      "lat": 28.6561592,
+      "lng": 77.2410203
+    }
+  },
+  {
+    "name": "V3s Mall",
+    "venue_id": "4bf8e148d4cdb713a2db84fe",
+    "location":{
+      "lat": 28.6373334,
+      "lng": 77.28639459999999
+    }
+  },
+  {
+    "name": "Cross River Mall",
+    "venue_id": "4ff59703e4b04619c6a7eb06",
+    "location":{
+      "lat": 28.6573654,
+      "lng": 77.3024234
+    }
+  },
+  {
+    "name": "Jantar Mantar",
+    "venue_id": "4befd1bdc8d920a132289430",
+    "location":{
+      "lat": 28.6270547,
+      "lng": 77.21662669999999
+    }
+  },
+  {
+    "name": "Cha Bar",
+    "venue_id": "51690773e4b09433491d990e",
+    "location":{
+      "lat": 28.6311389,
+      "lng": 77.2220528
+    }
+  },
+  {
+    "name": "Jama Masjid",
+    "venue_id": "4b529b68f964a520b48327e3",
+    "location":{
+      "lat": 28.6506792,
+      "lng": 77.23344209999999
+    }
+  },
+  {
+    "name": "Science Center",
+    "venue_id": "4dfadef31fc7c9f0b93a74a3",
+    "location":{
+      "lat": 28.6132098,
+      "lng": 77.245437
+    }
+  },
+  {
+    "name": "Akshardham",
+    "venue_id": "4b86304df964a5202d8231e3",
+    "location":{
+      "lat": 28.6126735,
+      "lng": 77.2772619
+    }
+  }
+  ,
+  {
+    "name": "kashmiri Gate",
+    "venue_id": "4c037ffb9a7920a140fbcf79",
+    "location":{
+      "lat": 28.6665706,
+      "lng": 77.2290677
+    }
+  }
+];
